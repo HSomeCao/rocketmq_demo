95d2f58d4b943b40158554eccc4942312f1399c4
caoliangwei <1345398664@qq.com>
2019-05-22 14:53:15 +0800

    新建消息模块（spring boot）

---
+++ bank_message/src/main/java/com/ydhy/bank_message/BankMessageApplication.java
@@ -0,0 +1,13 @@
+package com.ydhy.bank_message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankMessageApplication.class, args);
+	}
+
+}
