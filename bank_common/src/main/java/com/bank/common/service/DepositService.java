d19a29ea29c9963741c4eb0de3f2fd61f945a499
caoliangwei <1345398664@qq.com>
2019-05-22 17:15:34 +0800

    新建银行消息模块

--- bank_common/src/main/java/com/bank/common/service/DepositService.java
+++ bank_common/src/main/java/com/bank/common/service/DepositService.java
@@ -1,5 +1,7 @@
 package com.bank.common.service;
 
+import com.bank.common.pojo.Card;
+
 public interface DepositService {
 
 	
@@ -8,6 +10,8 @@ public interface DepositService {
 	String test2(String string);
 	
 	String test3(Integer i);
+
+	Card getObjectTest();
 //	Messages get(String num);
 //	
 //	Messages get(String num);
