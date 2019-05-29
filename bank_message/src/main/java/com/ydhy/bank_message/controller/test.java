6da23602af0207afc48a03a5b69c234b459ddac4
caoliangwei <1345398664@qq.com>
2019-05-29 16:27:17 +0800

    消息测试

--- bank_message/src/main/java/com/ydhy/bank_message/controller/test.java
+++ bank_message/src/main/java/com/ydhy/bank_message/controller/test.java
@@ -1,7 +1,24 @@
 package com.ydhy.bank_message.controller;
 
+import com.bank.common.info.MQResult;
+import com.bank.common.mqservice.ProducerSer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by caoliangwei on  2019/5/27 10:50
  */
+@RestController
+@RequestMapping("/message")
 public class test {
+    @Autowired(required = false)
+    ProducerSer producerSer;
+
+    @RequestMapping("/AsyncP")
+    @ResponseBody
+    MQResult testAsyncP(){
+       return producerSer.AsyncProducer();
+    }
 }
--- bank_message/src/main/java/com/ydhy/bank_message/service/impl/ProducerSerImpl.java
+++ bank_message/src/main/java/com/ydhy/bank_message/service/impl/ProducerSerImpl.java
@@ -2,19 +2,31 @@ package com.ydhy.bank_message.service.impl;
 
 import com.bank.common.info.MQResult;
 import com.bank.common.mqservice.ProducerSer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.io.Serializable;
+import java.math.BigDecimal;
 
 /**
  * Created by caoliangwei on  2019/5/27 16:27
  */
+@Service
 public class ProducerSerImpl implements ProducerSer {
 
     @Resource
     private RocketMQTemplate rocketMQTemplate;
+
     @Override
     public MQResult AsyncProducer() {
+        rocketMQTemplate.convertAndSend("test-topic-1", "Hello, World!");
+        rocketMQTemplate.send("test-topic-1", MessageBuilder.withPayload("Hello, World! I'm from spring message").build());
+        rocketMQTemplate.convertAndSend("test-topic-2", new OrderPaidEvent("T_001", new BigDecimal("88.00")));
         return null;
     }
 
@@ -46,5 +58,13 @@ public class ProducerSerImpl implements ProducerSer {
     @Override
     public MQResult TransactionProducer() {
         return null;
+    }
+
+    @Data
+    @AllArgsConstructor
+    public class OrderPaidEvent implements Serializable {
+        private String orderId;
+
+        private BigDecimal paidMoney;
     }
 }
